4f427bfaef725bbf0a9ce66808a5afc875ca4b99
AntoPerez123 <ant.perezp@duocuc.cl>
2026-04-08 20:48:07 -0400

    Add IAM role and instance profile for EC2, and configure VPC flow logs

--- Infra/vpc.tf
+++ Infra/vpc.tf
@@ -32,3 +32,34 @@ resource "aws_security_group" "sg" {
   }
 }
 
+resource "aws_cloudwatch_log_group" "vpc_log_group" {
+  name = "/aws/vpc/flowlogs"
+}
+
+resource "aws_iam_role" "flow_log_role" {
+  name = "flow-log-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "vpc-flow-logs.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_vpc_flow_log" "flow_log" {
+  vpc_id = aws_vpc.main.id
+  log_destination = aws_cloudwatch_log_group.vpc_log_group.arn
+  iam_role_arn    = aws_iam_role.flow_log_role.arn
+  traffic_type    = "ALL"
+}
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.main.id
+
+  ingress = []
+  egress  = []
+}
